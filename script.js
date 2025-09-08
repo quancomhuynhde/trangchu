f7e90dac8987352761b1a41d3083d5f80ca1324b
quancomhuynhde <akingan8@gmail.com>
2025-09-08 15:27:56 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ window.addEventListener("load", () => {
     const preloader = document.getElementById("preloader");
     preloader.style.opacity = "0";
     preloader.style.visibility = "hidden";
-    setTimeout(() => preloader.remove(), 800);
+    setTimeout(() => preloader.remove(), 500);
 });
 
 const btn = document.getElementById('btnOrder');
@@ -80,4 +80,5 @@ menuItems.forEach(item => {
             contactSection.scrollIntoView({ behavior: 'smooth' });
         }
     });
+
 });
